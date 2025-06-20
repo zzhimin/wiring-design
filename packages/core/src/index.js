74bbca677c659554fef5c56f8643d10283ccdd7f
zzhimin <1002588003@qq.com>
2025-06-20 18:13:22 +0800

    11

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,4 +1,4 @@
-import { Graph, Shape } from "@antv/x6";
+import { Graph } from "@antv/x6";
 import { Stencil } from "@antv/x6-plugin-stencil";
 import { Clipboard } from "@antv/x6-plugin-clipboard";
 import { Selection } from "@antv/x6-plugin-selection";
